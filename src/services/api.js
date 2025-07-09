c47a10f0f81fda738e5279c271d228eac60dbdb9
chintha rithwik <chintharithwik77@gmail.com>
2025-07-09 19:01:59 +0530

    production

--- src/services/api.js
+++ src/services/api.js
@@ -6,6 +6,44 @@ if (!API_KEY) {
 }
 
 // Mock data for testing
+const mockMovies = [
+    {
+        id: 1,
+        title: "Avengers: Endgame",
+        release_date: "2019-04-26",
+        poster_path: "/or06FN3Dka5tukK1e9sl16pB3iy.jpg"
+    },
+    {
+        id: 2,
+        title: "Spider-Man: No Way Home",
+        release_date: "2021-12-17",
+        poster_path: "/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg"
+    },
+    {
+        id: 3,
+        title: "Avatar: The Way of Water",
+        release_date: "2022-12-16",
+        poster_path: "/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+    },
+    {
+        id: 4,
+        title: "Black Panther",
+        release_date: "2018-02-16",
+        poster_path: "/uxzzxijgPIY7slzFvMotPv8wjKA.jpg"
+    },
+    {
+        id: 5,
+        title: "The Batman",
+        release_date: "2022-03-04",
+        poster_path: "/b0PlSFdDwbyK0cf5RxwDpaOJQvQ.jpg"
+    },
+    {
+        id: 6,
+        title: "Top Gun: Maverick",
+        release_date: "2022-05-27",
+        poster_path: "/62HCnUTziyWcpDaBO2i1DX17ljH.jpg"
+    }
+];
 
 
 export const getPopularMovies=async()=>{
